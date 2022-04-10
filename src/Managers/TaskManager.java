4c32c456678995c9c9395ca854d9b8637c53c35d
GGk77 <kirill.rekuts@yandex.ru>
2022-04-10 15:12:56 +0300

    Сделал класс интерфейсом, перенес в пакет Managers

--- src/Managers/TaskManager.java
+++ src/Managers/TaskManager.java
@@ -1,2 +1,49 @@
-package Managers;public interface TaskManager {
+package Managers;
+
+import Tasks.Epic;
+import Tasks.Sub;
+import Tasks.Task;
+
+import java.util.List;
+
+public interface TaskManager {
+
+    // Получаем список всех задач.
+    List<Task> getTasks();
+    List<Epic> getEpics();
+    List<Sub> getSubs();
+    Task getTask(long id);
+    Epic getEpic(long id);
+    Sub getSub(long id);
+
+    // Создание
+    Long addTask(Task task);
+    Long addEpic(Epic epic);
+    Long addSubTask(Sub sub);
+
+    // Получение по ID
+    void getTaskById(long id);
+
+    // Получение подзадач эпика
+    List<Sub> getSubtaskEpic(long epicId);
+
+    // Обновление
+    void updateTask(Task task);
+    void updateEpic(Epic epic);
+    void updateSub(Sub sub);
+
+    // Генератор ID
+    Long idGenerate();
+
+    // Удаление
+    void removeAllTasks();
+    void deleteTaskByIdSingle(long id);
+    void deleteTaskByIdEpic(long id);
+    void deleteTaskByIdSubtask(long id);
+
+    // Обновление статуса Epic
+    void updateEpicStatus(long epicId);
+
+    // Получение истории
+    List<Task>getHistory();
 }
