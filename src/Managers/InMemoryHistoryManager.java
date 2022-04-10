9c49f520d4da23dd0559c3d3d34379472fe61b5c
GGk77 <kirill.rekuts@yandex.ru>
2022-04-10 15:08:45 +0300

    Сделал класс InMemoryHistoryManager, имплеметировался от HistoryManager, добавил реализацию методов, добавил в пакет Managers

--- src/Managers/InMemoryHistoryManager.java
+++ src/Managers/InMemoryHistoryManager.java
@@ -1,2 +1,28 @@
-package Managers;public class InMemoryHistoryManager {
+package Managers;
+
+import Tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class InMemoryHistoryManager  implements HistoryManager {
+
+    private final List<Task> history = new ArrayList<>();
+
+    @Override
+    public List<Task> getHistory() {
+        return history;
+    }
+
+    @Override
+    public void addTask(Task task) {
+        if(Objects.isNull(task)) {
+            return;
+        }
+        history.add(task);
+        if (history.size() > 10) {
+            history.remove(0);
+        }
+    }
 }
