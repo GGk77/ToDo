5ee9955803376afd5db280cb8ec8869a26bc7887
GGk77 <kirill.rekuts@yandex.ru>
2022-04-23 00:44:07 +0300

    Добавил реализацию методов removeNode, linkLast, addTask, getTasks, Node

--- src/Managers/InMemoryHistoryManager.java
+++ src/Managers/InMemoryHistoryManager.java
@@ -3,29 +3,95 @@ package Managers;
 import Tasks.Task;
 
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-public class InMemoryHistoryManager  implements HistoryManager {
+public class InMemoryHistoryManager implements HistoryManager {
 
-    private final long LENGTH = 10;
-
-    private final LinkedList<Task> history = new LinkedList<>();
+    private final HashMap<Long, Node> nodes = new HashMap<>();
+    private Node head;
+    private Node tail;
 
     @Override
     public List<Task> getHistory() {
-        return history;
+        return getTasks();
+    }
+
+    private List<Task> getTasks() {
+        List<Task> historyList = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            historyList.add(node.getTask());
+            node = node.next;
+        }
+        return historyList;
     }
 
     @Override
     public void addTask(Task task) {
-        if(Objects.isNull(task)) {
+        if (Objects.isNull(task)) {
             return;
         }
-        history.add(task);
-        if (history.size() > LENGTH) {
-            history.removeFirst();
+        final Long id = task.getId();
+        removeNode(id);
+        linkLast(task);
+        nodes.put(id, tail);
+    }
+
+    @Override
+    public void remove(Long id) {
+            removeNode(id);
+
+    }
+
+    private void linkLast(Task task) {
+        final Node node = new Node(task, null, tail);
+        if (head == null) {
+            head = node;
+        } else {
+            tail.next = node;
+        }
+        tail = node;
+    }
+
+    private void removeNode(Long id) {
+        Node node = this.nodes.get(id);
+        if (node != null) {
+            if (node.next == null) {
+                tail = node.prev;
+
+            } else if (node.prev == null) {
+                head = node.next;
+
+            } else {
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+
+            }
+        }
+    }
+
+    private static class Node {
+        Task task;
+        Node next;
+        Node prev;
+
+        public Node(Task task, Node next, Node prev) {
+            this.task = task;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public Task getTask() {
+            return task;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "task=" + task +
+                    ", next=" + next +
+                    ", prev=" + prev +
+                    '}';
         }
     }
 }
